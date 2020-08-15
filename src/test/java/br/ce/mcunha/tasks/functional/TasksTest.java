651279ba3a0ee2e5ba0cc183f76c59f897f138bf
marcosfacjf <marcos.cunha@squadra.com.br>
2020-08-15 03:39:51 -0300

    Adjust version

--- src/test/java/br/ce/mcunha/tasks/functional/TasksTest.java
+++ src/test/java/br/ce/mcunha/tasks/functional/TasksTest.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class TasksTest {
 	
 	public WebDriver acessarAplicacao() {
+		System.setProperty("webdriver.chrome.driver","C:\\Driver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.navigate().to("http://localhost:8001/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
